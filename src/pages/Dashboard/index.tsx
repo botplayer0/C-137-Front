be38a0cbebd3eab0d66dd94d9398337bca3b0015
arctic-lin <arctic1973@163.com>
2023-04-10 20:05:47 +0800

    feature: 导航栏与Tab页的联动

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,18 @@
 import { routeConfig } from "@/consts/routeConfig";
-import { Layout, Menu } from "antd";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import { Grid, Layout, Menu } from "antd";
 import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { CMenu } from "./components/CMenu";
 import CTabs from "./components/CTabs";
 
+import "./index.css";
+
 const { Sider, Content } = Layout;
 const { SubMenu } = Menu;
 
 function MyLayout() {
+  const { useBreakpoint } = Grid;
   const { route } = routeConfig;
   const navigate = useNavigate();
   // 控制Menu的选择 + tab的选中
@@ -17,6 +21,12 @@ function MyLayout() {
   const [openKeys, setOpenKeys] = useState([]);
   // tab的展开与关闭
   const [tabsItem, setTabsItem] = useState([]);
+  // 控制菜单展开和收合
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
 
   useEffect(() => {
     // 初始化进来后展开管理页-我的和打开tab
@@ -28,7 +38,11 @@ function MyLayout() {
   const handleMenuClick = (path: string) => {
     // Temp: 展开MenuGroup
     let pathSplit = path.split("/");
-    if (pathSplit.length && openKeys.indexOf(`/${pathSplit[1]}`) === -1) {
+    if (
+      pathSplit.length &&
+      openKeys.indexOf(`/${pathSplit[1]}`) === -1 &&
+      !collapsed
+    ) {
       setOpenKeys([...openKeys, `/${pathSplit[1]}`]);
     }
     // 控制Menu选择与Tabs的选中,双向
@@ -75,11 +89,33 @@ function MyLayout() {
     }
     setTabsItem(newTabsItem);
   };
-  console.log("Open", openKeys);
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Sider>
-        <div>22</div>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        style={{ position: "relative" }}
+        breakpoint="md"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+          setCollapsed(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div>Logo</div>
+        <div style={{ position: "absolute", top: 0, right: 0, height: "100%" }}>
+          <div
+            onClick={toggleCollapsed}
+            className="collapse-btn"
+            style={{ zIndex: 3 }}
+          >
+            {collapsed ? <RightOutlined /> : <LeftOutlined />}
+          </div>
+        </div>
+
         <br />
         <br />
         <CMenu
@@ -91,7 +127,7 @@ function MyLayout() {
           onOpenChange={handleMenuGroupClick}
         />
       </Sider>
-      <Layout>
+      <Layout style={{ zIndex: 1 }}>
         <Content>
           <CTabs
             items={tabsItem}
